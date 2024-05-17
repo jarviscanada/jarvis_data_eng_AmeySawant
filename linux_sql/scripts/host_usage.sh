7e670cf086c49b6ea68660c5357ba4ebfbda7c5e
connectamey25 <168485628+connectamey25@users.noreply.github.com>
2024-05-16 22:29:23 -0400

    Update host_usage.sh

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -22,7 +22,7 @@ disk_available=$(df -BM / | tail -1 | awk '{print $4}' | sed 's/.$//')
 
 # Retrieve host_id from host_info table based on the hostname
 hostname=$(hostname -f)
-host_id=$(psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -t -c "SELECT id FROM host_info WHERE hostname = '$hostname';")
+host_id=$(psql -h "$psql_host" -p "$psql_port" -d "$db_name" -U "$psql_user" -t -c "SELECT id FROM host_info WHERE hostname = '$hostname';")
 
 # Check if host_id is null
 if [ -z "$host_id" ]; then
@@ -30,10 +30,17 @@ if [ -z "$host_id" ]; then
     exit 1
 fi
 
+# Debugging output
+echo "Hostname: $hostname"
+echo "Host ID: $host_id"
+
 # Construct the INSERT statement with the correct host_id value
 insert_stmt="INSERT INTO host_usage (timestamp, host_id, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES ('$timestamp', $host_id, $cpu_idle, $cpu_kernel, $disk_io, $disk_available);"
 
+# Debugging output
+echo "Insert Statement: $insert_stmt"
+
 # Execute the INSERT statement
-export PGPASSWORD=$psql_password 
-psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+export PGPASSWORD="$psql_password" 
+psql -h "$psql_host" -p "$psql_port" -d "$db_name" -U "$psql_user" -c "$insert_stmt"
 exit $?
